894f03002877ae25fbb95e495aff84b4c9931f1d
aredev <appierasool@gmail.com>
2016-04-17 14:02:46 +0200

    XML Parser added

--- src/eap_peer/eap_md5.d
+++ src/eap_peer/eap_md5.d
@@ -13,4 +13,22 @@
  /home/aredev/Documents/wpa_supplicant-2.5/src/eap_common/eap_defs.h \
  /home/aredev/Documents/wpa_supplicant-2.5/src/eap_peer/eap_methods.h \
  /home/aredev/Documents/wpa_supplicant-2.5/src/eap_common/eap_common.h \
- /home/aredev/Documents/wpa_supplicant-2.5/src/eap_common/chap.h
+ /home/aredev/Documents/wpa_supplicant-2.5/src/eap_common/chap.h \
+ /usr/include/libxml2/libxml/parser.h \
+ /usr/include/libxml2/libxml/xmlversion.h \
+ /usr/include/libxml2/libxml/xmlexports.h \
+ /usr/include/libxml2/libxml/tree.h \
+ /usr/include/libxml2/libxml/xmlstring.h \
+ /usr/include/libxml2/libxml/xmlregexp.h \
+ /usr/include/libxml2/libxml/dict.h /usr/include/libxml2/libxml/hash.h \
+ /usr/include/libxml2/libxml/valid.h \
+ /usr/include/libxml2/libxml/xmlerror.h \
+ /usr/include/libxml2/libxml/list.h \
+ /usr/include/libxml2/libxml/xmlautomata.h \
+ /usr/include/libxml2/libxml/entities.h \
+ /usr/include/libxml2/libxml/encoding.h \
+ /usr/include/libxml2/libxml/xmlIO.h \
+ /usr/include/libxml2/libxml/globals.h /usr/include/libxml2/libxml/SAX.h \
+ /usr/include/libxml2/libxml/xlink.h /usr/include/libxml2/libxml/SAX2.h \
+ /usr/include/libxml2/libxml/xmlmemory.h \
+ /usr/include/libxml2/libxml/threads.h
